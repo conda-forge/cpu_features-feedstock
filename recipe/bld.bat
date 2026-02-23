62e3b7e5b55c87ccda55d64ba4d6171ace20e9d4
Michael R. Crusoe <michael.crusoe@gmail.com>
2026-02-23 18:54:41 +0100

    work around CMAKE minimum version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 mkdir build%vc%
 cd build%vc%
 cd
-cmake -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON ..
 if errorlevel 1 exit 1
 cmake --build . --target install --config Release
